39daf6353e46415a06e94a42cb54908403890612
allenbacunawa2007-rgb <allenbacunawa2007@gmail.com>
2026-05-08 10:44:22 +0800

    map loader update, and tiles imported

--- Source/MapLoader.java
+++ Source/MapLoader.java
@@ -7,22 +7,28 @@ public class MapLoader {
 
     private static final String filepath = "Assets/Maps/Map-save-updated-final.txt";
 
-    public static String[][] loadMap() {
+    public static int[][] loadMap() {
 
-        ArrayList<String[]> rows = new ArrayList<>();
+        ArrayList<int[]> rows = new ArrayList<>();
 
         try (BufferedReader BR = new BufferedReader(new FileReader(filepath))) {
             String line;
 
             while ((line = BR.readLine()) != null) {
                 String[] values = line.split(",");
-                rows.add(values);
+
+                int[] row = new int[values.length];
+
+                for (int i = 0; i < values.length; i++) {
+                    row[i] = Integer.parseInt(values[i]);
+                }
+                rows.add(row);
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return rows.toArray(new String[0][]);
+        return rows.toArray(new int[0][]);
     }
 }
