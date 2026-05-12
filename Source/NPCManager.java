4d35c0d0b41968507301aa0bfd6f93c92379cb29
randomshi <randomshi.com>
2026-05-12 16:43:10 +0800

    sdfd

--- Source/NPCManager.java
+++ Source/NPCManager.java
@@ -23,7 +23,29 @@ public class NPCManager {
         npc[3].addPokeRot(gamePanel.pokeRotRegistry.getSpecificPokeRot("Boneca Ambalabu"));
         npc[4].addPokeRot(gamePanel.pokeRotRegistry.getSpecificPokeRot("Brr Brr Patapim"));
 
-        npc[0].setDialogues(new String[]{"hello there", "yoou da real nigga"}, new String[]{"damn you beat me bro"});
+        npc[0].setDialogues(
+                new String[] { "Hello there, Carl!", "Let's see how well you've trained your PokeRots!" },
+                new String[] { "Impressive tracking metrics!", "You're ready to explore the rest of the campus core." });
+
+        // npc[1] - Trainer_Peej
+        npc[1].setDialogues(
+                new String[] { "Heh, you stepped right into my vision field!", "Prepare to get completely flushed!" },
+                new String[] { "Womp womp...", "Your synergy architecture is unironically clear." });
+
+        // npc[2] - Trainer_Allen
+        npc[2].setDialogues(
+                new String[] { "Are your algorithms optimized for combat?", "Let's run a live test scenario!" },
+                new String[] { "Ah, a critical execution exception...", "Take this loss confirmation token." });
+
+        // npc[3] - Rival_Kazuya
+        npc[3].setDialogues(
+                new String[] { "Took you long enough to trace my location, Carl.", "My custom party builds are strictly tier-one.", "Let's settle this calculation right now!" },
+                new String[] { "Tsk, a simple rounding variance error.", "Don't get overly confident, I'm already refactoring my assets." });
+
+        // npc[4] - Champ_Reese
+        npc[4].setDialogues(
+                new String[] { "Welcome to the server core.", "Only absolute peak compilation states survive here.", "Show me your data structures!" },
+                new String[] { "Magnificent deployment process!", "You have completely viralized the local network ecosystem." });
         
 
     }
